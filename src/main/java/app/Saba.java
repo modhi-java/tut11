978e8ee6da488d1a6773567084d4f415ea1bade1
fahed-java <almohob.com@hotmail.com>
2018-04-08 22:50:30 +0300

    x++

--- src/main/java/app/Saba.java
+++ src/main/java/app/Saba.java
@@ -5,9 +5,41 @@ public class Saba {
  // والسطر الثالث لو كتبنا البلس بلس يمين يطبع نتيجة السطرين
  // ولو العكس بلس بلس من اليسار يجمع واحد على السطرين , لأن السطر الأخير أصلا طباعة
     public static void main (String[] jj){
-        int x = 5 ;
-        x++;
-      System.out.println(x++);
+//        int x = 5 ;
+//        x++;
+//      System.out.println(x++);
+
+
+        // المقارنات , قاعدة اليساوي
+//int x = 10;
+//int y = 20;
+//System.out.println(x==y);
+
+        // علامة التعجب بجنب اليساوي معناها لاتساوي
+//        int x = 10;
+//        int y = 20;
+//        System.out.println(x!=y);
+
+        // هل أكس أكبر من واي
+//        int x = 10;
+//        int y = 20;
+//        System.out.println(x>y);
+
+        // هل أكس أصغر من واي
+//        int x = 10;
+//        int y = 20;
+//        System.out.println(x<y);
+
+        // هل أكس أكبر من أو يساوي واي
+//        int x = 10;
+//        int y = 20;
+//        System.out.println(x>=y);
+
+        // هل أكس أصغر من أو يساوي واي
+        int x = 10;
+        int y = 20;
+        System.out.println(x<=y);
 
     }
+
 }
