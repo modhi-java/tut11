3e26b65b0a67798ab046d29cf2dc30f071ed5c1f
modhi-java <Modhi2585>
2018-04-24 22:44:59 +0300

    شرح الاستثناءات2

--- src/main/java/app/Tut13_2.java
+++ src/main/java/app/Tut13_2.java
@@ -4,20 +4,17 @@ public class Tut13_2 {
 
     public static void main(String[] args) {
         // مما يعني أنها ستقوم برمي إستثناء argument هنا قمنا باستدعاء الدالة و أعطيناها القيمة 70 كـ
-        checkAge(70);
+        try {
+            checkAge(70);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
-    public static void checkAge (int age) {
-        // و النص الذي وضعناه بين القوسين عبارة عن شرح للمشكلة التي قد تحدث و هو الذي سيتم عرضه catch أكبر من 63 سيتم رمي إستثناء إلى الدالة age هنا الدالة تقوم بتجربة الكود و في حال كانت قيمة
-        try {
-            if(age > 63) {
-                throw new Exception("أنت كبير في السن");
-            }
-        }
-        // ثم تقوم بعرض نوع الخطأ e تلتقط أي إستثناء قد يحدث, بعدها تقوم بتخزينه في الكائن catch هذه الـ
-        catch( Exception e ) {
-            System.out.println( e.getMessage() );
+    public static void checkAge (int age) throws Exception {
+        if(age > 63) {
+            throw new Exception("أنت كبير في السن");
         }
     }
 
